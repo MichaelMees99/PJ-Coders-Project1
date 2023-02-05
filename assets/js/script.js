650d7a59cdd712aa8c00f2be77f5dea784586379
Michael Mees <Michaelmees99@gmail.com>
2023-02-04 21:27:58 -0500

    Initial Movie API and linked html to Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "29318d71f8msh332848e52019318p11c75ajsn9867f857f124",
+		"X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+console.log(settings)
